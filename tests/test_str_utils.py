d02c496cf20e531f0af801c672ae40dfce3745ab
AndresAyala <andres.ayala@student.ie.edu>
2022-01-21 10:52:45 +0000

    last remarks

--- tests/test_str_utils.py
+++ tests/test_str_utils.py
@@ -1,22 +1,23 @@
 # Filename has to start with test
 
-#def test_dummy():
-    # You can assert for whatever you want,
-    # *but* people usually avoid them in production code
-    # assert [1,2]==[1,2,3]
-    #assert True
+# def test_dummy():
+# You can assert for whatever you want,
+# *but* people usually avoid them in production code
+# assert [1,2]==[1,2,3]
+# assert True
 
 from titanic_utils.str_utils import extract_titles
 
 import pytest
 
-def test_extract_titles_returns_expected_output():
-    #1.Preconditions
-    expected_input = "Mr. John Doe"  #what I am using as input
-    expected_output = "Mr."           #what I expect ro recieve
-
-    #2.Run the code
+# @ = "Decorator"
+@pytest.mark.parametrize(
+    "expected_input, expected_output",
+    [("Mr. John Doe", "Mr."), ("Miss. Jane Doe", "Miss."), ("Dr. Manhattan", "Dr.")],
+)
+def test_extract_titles_returns_expected_output(expected_input, expected_output):
+    # 2.Run the code
     output = extract_titles(expected_input)
 
-    #3. Verify
+    # 3. Verify
     assert output == expected_output
